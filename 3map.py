748c37bdb1bdb886a20b4cc319e44bd627a736ca
Gabrielcamisadez <gabriel448521@gmail.com>
2025-03-19 00:15:07 -0300

    .

---
+++ 3map.py
@@ -0,0 +1,34 @@
+import asyncio
+
+async def scan_network(ip_range):
+    print(f"🔎 Iniciando scan na rede {ip_range}... Aguarde.")
+
+    process = await asyncio.create_subprocess_exec(
+        "nmap", "-sn", ip_range,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+
+    stdout, stderr = await process.communicate()
+
+    if process.returncode == 0:
+        print(f"✅ Processando resultados: {ip_range}.")
+
+        for line in stdout.decode().splitlines():
+            if "Nmap scan report for" in line:
+                ip = line.split()[-1].strip("()")
+                print(f"🎯 Host ativo encontrado: {ip}")
+                await asyncio.sleep(0.5) 
+    else:
+        print(f" Erro ao escanear a rede {ip_range}: {stderr.decode()}")
+
+    print(f"🏆 Finish!")
+
+async def main():
+    ip_ranges = ["201.49.158.0/23"]
+
+    tasks = [scan_network(ip_range) for ip_range in ip_ranges]
+
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
