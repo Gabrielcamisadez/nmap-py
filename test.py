0702536f2e6a0e63816dbd76edacb9187fa7f12c
Gabrielcamisadez <129340371+Gabrielcamisadez@users.noreply.github.com>
2025-07-13 15:26:25 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 import socket
 
 def check_connectivity(ip_range):
-    """
-    Verifica a conectividade com um intervalo de endereços IP.
-    """
     for ip in ip_range:
         try:
-            # Tenta criar um socket para o endereço IP na porta 80 (HTTP)
             socket.setdefaulttimeout(1)
             socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((ip, 80))
             print(f"Conectividade detectada com {ip}")
@@ -14,5 +10,5 @@ def check_connectivity(ip_range):
             print(f"Não foi possível conectar a {ip}")
 
 if __name__ == "__main__":
-    ip_range = ["8.8.8.8", "1.1.1.1", "8.8.4.4"]  # Exemplo de intervalo de IPs
+    ip_range = ["8.8.8.8", "1.1.1.1", "8.8.4.4"] 
     check_connectivity(ip_range)
